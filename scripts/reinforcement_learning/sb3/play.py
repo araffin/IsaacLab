Fix loading of agent and normalization

scripts/reinforcement_learning/sb3/play.py
@@ -165,6 +165,12 @@ def main():
     vec_norm_path = Path(vec_norm_path)
 
     logging.getLogger().setLevel(logging.INFO)
+    norm_keys = {"normalize_input", "normalize_value", "clip_obs"}
+    norm_args = {}
+    for key in norm_keys:
+        if key in agent_cfg:
+            norm_args[key] = agent_cfg.pop(key)
+
     # normalize environment (if needed)
     if vec_norm_path.exists():
         print(f"Loading saved normalization: {vec_norm_path}")
@@ -173,13 +179,16 @@ def main():
         env.training = False
         # reward normalization is not needed at test time
         env.norm_reward = False
-    elif "normalize_input" in agent_cfg:
+    elif norm_args and norm_args.get("normalize_input"):
         print("Relearning normalization")
         env = VecNormalize(
             env,
             training=True,
-            norm_obs="normalize_input" in agent_cfg and agent_cfg.pop("normalize_input"),
-            clip_obs="clip_obs" in agent_cfg and agent_cfg.pop("clip_obs"),
+            norm_obs=norm_args["normalize_input"],
+            norm_reward=False, # Do not normalize reward at test time
+            clip_obs=norm_args.get("clip_obs", 100.0),
+            gamma=agent_cfg["gamma"],
+            clip_reward=np.inf,
         )
 
     # create agent from stable baselines

scripts/reinforcement_learning/sb3/train.py
@@ -378,7 +378,12 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     # agent_cfg["policy_kwargs"]["ortho_init"] = True
     # agent_cfg["policy_kwargs"]["log_std_init"] = -0.5
     # agent_cfg["policy_kwargs"]["net_arch"] = [1024, 512, 256]
-
+    # agent_cfg["param_resets"] = [int(i * 4e7) for i in range(1, 10)]
+    # agent_cfg["policy_kwargs"]["squash_output"] = squash_output
+    # agent_cfg["policy_kwargs"]["optimizer_class"] = optax.adam
+    # agent_cfg["policy_kwargs"]["optimizer_kwargs"] = {"eps": 1e-5}
+    # agent_cfg["policy_kwargs"]["ortho_init"] = True
+    #
     if args_cli.hyperparams is not None:
         print("Updating hyperparams from cli")
         agent_cfg.update(args_cli.hyperparams)
@@ -405,18 +410,11 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
             env,
             training=True,
             norm_obs=norm_args["normalize_input"],
-            norm_reward=norm_args.get("normalize_input", False),
+            norm_reward=norm_args.get("normalize_value", False),
             clip_obs=norm_args.get("clip_obs", 100.0),
             gamma=agent_cfg["gamma"],
             clip_reward=np.inf,
         )
-
-    # agent_cfg["param_resets"] = [int(i * 4e7) for i in range(1, 10)]
-    # agent_cfg["policy_kwargs"]["squash_output"] = squash_output
-    # agent_cfg["policy_kwargs"]["optimizer_class"] = optax.adam
-    # agent_cfg["policy_kwargs"]["optimizer_kwargs"] = {"eps": 1e-5}
-    # agent_cfg["policy_kwargs"]["ortho_init"] = True
-    # agent_cfg["policy_kwargs"]["net_arch"] = [512, 256, 128]
 
     # Sort for printing
     hyperparams = {key: agent_cfg[key] for key in sorted(agent_cfg.keys())}